36f18a69e9fe01dc07faca483915d3bbc1bf31c4
Junyi Zheng <zhengjunyi@unm.edu>
2022-04-16 00:27:17 +0000

    Upload New File

---
+++ src/util/TextExport.java
@@ -0,0 +1,293 @@
+package util;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class TextExport {
+
+	private static String path = "../conf.txt";
+	private static String filenameTemp;
+
+
+	
+	
+	/**
+     * 功能：Java读取txt文件的内容
+     * 步骤：1：先获得文件句柄
+     * 2：获得文件句柄当做是输入一个字节码流，需要对这个输入流进行读取
+     * 3：读取到输入流后，需要读取生成字节流
+     * 4：一行一行的输出。readline()。
+     * 备注：需要考虑的是异常情况
+     * @param filePath
+     */
+    public static Map readTxtFile(String filePath){
+    	HashMap<String,String> retmap= new HashMap<String,String> ();
+        try {
+                String encoding="UTF-8";
+                File file=new File(filePath);
+                if(file.isFile() && file.exists()){ //判断文件是否存在
+                    InputStreamReader read = new InputStreamReader(
+                    new FileInputStream(file),encoding);//考虑到编码格式
+                    BufferedReader bufferedReader = new BufferedReader(read);
+                    String lineTxt = null;
+                    while((lineTxt = bufferedReader.readLine()) != null){
+                        System.out.println(lineTxt);
+                        String[] arr=lineTxt.split("=");
+                        if(arr!=null) {
+                        	retmap.put(arr[0].trim(), arr[1].trim());
+                        }
+                    }
+                    read.close();
+        }else{
+            System.out.println("can't find file");
+        }
+        } catch (Exception e) {
+            System.out.println("read file content error");
+            e.printStackTrace();
+        }
+     return retmap;
+    }
+     
+    public static void main(String argv[]){
+        String filePath = TextExport.class.getResource("../conf.txt").getPath();
+        System.out.println(filePath);
+//      "res/";
+        Map<String,String> map=TextExport.readTxtFile(filePath);
+        System.out.println(map.toString());
+        System.out.println(map.get("dimHeight"));
+    }
+     
+	
+	/**
+	 * 创建文件
+	 * 
+	 * @throws IOException
+	 */
+	public static boolean creatTxtFile(String name) throws IOException {
+		boolean flag = false;
+		filenameTemp = path + name + ".txt";
+		File filename = new File(filenameTemp);
+		if (!filename.exists()) {
+			filename.createNewFile();
+			flag = true;
+		}
+		return flag;
+	}
+
+	/**
+	 * 写文件
+	 * 
+	 * @param newStr
+	 *            新内容
+	 * @throws IOException
+	 */
+	public static boolean writeTxtFile(String newStr) throws IOException {
+		// 先读取原有文件内容，然后进行写入操作
+		boolean flag = false;
+		String filein = newStr + "\r\n";
+		String temp = "";
+
+		FileInputStream fis = null;
+		InputStreamReader isr = null;
+		BufferedReader br = null;
+
+		FileOutputStream fos = null;
+		PrintWriter pw = null;
+		try {
+			// 文件路径
+			File file = new File(filenameTemp);
+			// 将文件读入输入流
+			fis = new FileInputStream(file);
+			isr = new InputStreamReader(fis);
+			br = new BufferedReader(isr);
+			StringBuffer buf = new StringBuffer();
+
+			// 保存该文件原有的内容
+			for (int j = 1; (temp = br.readLine()) != null; j++) {
+				buf = buf.append(temp);
+				// System.getProperty("line.separator")
+				// 行与行之间的分隔符 相当于“\n”
+				buf = buf.append(System.getProperty("line.separator"));
+			}
+			buf.append(filein);
+
+			fos = new FileOutputStream(file);
+			pw = new PrintWriter(fos);
+			pw.write(buf.toString().toCharArray());
+			pw.flush();
+			flag = true;
+		} catch (IOException e1) {
+			// TODO 自动生成 catch 块
+			throw e1;
+		} finally {
+			if (pw != null) {
+				pw.close();
+			}
+			if (fos != null) {
+				fos.close();
+			}
+			if (br != null) {
+				br.close();
+			}
+			if (isr != null) {
+				isr.close();
+			}
+			if (fis != null) {
+				fis.close();
+			}
+		}
+		return flag;
+	}
+	
+	/**
+	 * 写文件
+	 * 
+	 * @param newStr
+	 *            新内容
+	 * @throws IOException
+	 */
+	public static boolean writeTxtFile(String filename,String newStr) throws IOException {
+		boolean flag = false;
+		filenameTemp = path + filename + ".txt";
+		File filenamenew = new File(filenameTemp);
+		if (!filenamenew.exists()) {
+			filenamenew.createNewFile();
+		}
+		
+		// 先读取原有文件内容，然后进行写入操作
+		String filein = newStr + "\r\n";
+		String temp = "";
+
+		FileInputStream fis = null;
+		InputStreamReader isr = null;
+		BufferedReader br = null;
+
+		FileOutputStream fos = null;
+		PrintWriter pw = null;
+		try {
+			// 文件路径
+			File file = new File(filenameTemp);
+			// 将文件读入输入流
+			fis = new FileInputStream(file);
+			isr = new InputStreamReader(fis);
+			br = new BufferedReader(isr);
+			StringBuffer buf = new StringBuffer();
+
+			// 保存该文件原有的内容
+			for (int j = 1; (temp = br.readLine()) != null; j++) {
+				buf = buf.append(temp);
+				// System.getProperty("line.separator")
+				// 行与行之间的分隔符 相当于“\n”
+				buf = buf.append(System.getProperty("line.separator"));
+			}
+			buf.append(filein);
+
+			fos = new FileOutputStream(file);
+			pw = new PrintWriter(fos);
+			pw.write(buf.toString().toCharArray());
+			pw.flush();
+			flag = true;
+		} catch (IOException e1) {
+			// TODO 自动生成 catch 块
+			throw e1;
+		} finally {
+			if (pw != null) {
+				pw.close();
+			}
+			if (fos != null) {
+				fos.close();
+			}
+			if (br != null) {
+				br.close();
+			}
+			if (isr != null) {
+				isr.close();
+			}
+			if (fis != null) {
+				fis.close();
+			}
+		}
+		return flag;
+	}
+	
+	
+	/**
+	 * 写文件
+	 * 
+	 * @param newStr
+	 *            新内容
+	 * @throws IOException
+	 */
+	public static boolean writeTxtFile(String filename,List<String> str) throws IOException {
+		boolean flag = false;
+		filenameTemp = filename;
+		File filenamenew = new File(filenameTemp);
+		if (!filenamenew.exists()) {
+			filenamenew.createNewFile();
+		}
+		
+		// 先读取原有文件内容，然后进行写入操作
+		String filein = "";//"\r\n";
+		String temp = "";
+
+		FileInputStream fis = null;
+		InputStreamReader isr = null;
+		BufferedReader br = null;
+
+		FileOutputStream fos = null;
+		PrintWriter pw = null;
+		try {
+			// 文件路径
+			File file = new File(filenameTemp);
+			// 将文件读入输入流
+			fis = new FileInputStream(file);
+			isr = new InputStreamReader(fis);
+			br = new BufferedReader(isr);
+			StringBuffer buf = new StringBuffer();
+
+			// 保存该文件原有的内容
+			for (int j = 0;j<str.size(); j++) {
+				buf = buf.append(str.get(j));
+				// System.getProperty("line.separator")
+				// 行与行之间的分隔符 相当于“\n”
+				buf = buf.append(System.getProperty("line.separator"));
+			}
+			buf.append(filein);
+
+			fos = new FileOutputStream(file);
+			pw = new PrintWriter(fos);
+			pw.write(buf.toString().toCharArray());
+			pw.flush();
+			flag = true;
+		} catch (IOException e1) {
+			// TODO 自动生成 catch 块
+			throw e1;
+		} finally {
+			if (pw != null) {
+				pw.close();
+			}
+			if (fos != null) {
+				fos.close();
+			}
+			if (br != null) {
+				br.close();
+			}
+			if (isr != null) {
+				isr.close();
+			}
+			if (fis != null) {
+				fis.close();
+			}
+		}
+		return flag;
+	}
+
+}
